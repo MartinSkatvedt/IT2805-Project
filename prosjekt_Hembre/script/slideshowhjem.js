7465185135dc0d33f58b59db3f0b2b7a1f3bf78a
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-10-09 11:33:31 +0200

    slideshow

--- prosjekt_Hembre/script/slideshowhjem.js
+++ prosjekt_Hembre/script/slideshowhjem.js
@@ -1,22 +1,41 @@
 let slideIndex = -1;
 showSlides();
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+next.addEventListener("click", test)
+prev.addEventListener("click", test)
+
+function test(event) {
+    eventType = event.target.className
+    if (eventType == 'next') {
+        nextSlide(1)
+    } else {
+        nextSlide(-1)
+    }
+}
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  let next = document.querySelector("next");
-  let prev = document.querySelector("prev");
+    nextSlide(1)
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+function nextSlide(n) {
+    const slides = document.getElementsByClassName("slides");
 
-  for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
-  }
-
-  slideIndex++;
-
-  if (slideIndex >= slides.length) {
-      slideIndex = 0
     }
-  slides[slideIndex].style.display = "block";  
-  //dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
+
+    slideIndex += n
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0
+    }
+    if (slideIndex < 0) {
+        slideIndex = slides.length-1
+    }
+    console.log(slideIndex);
+    
+    slides[slideIndex].style.display = "block";  
+    
 }
