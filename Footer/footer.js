bb6650253fa0f75feac3d7d186ff75318e2c4a47
Casper Amtrup <casperamtrup@Yoink.local>
2020-10-21 22:27:09 +0200

    endret objekt.js

---
+++ Footer/footer.js
@@ -0,0 +1,84 @@
+document.write(`
+    <footer>
+
+      <div class="footer-container-one">
+
+        <img src="logo" alt="">
+
+        <div class="listePaal">
+
+          <ul>
+            <li><p><a href="#">Pål Hembre</a></p></li>
+            <li><a href="#">Best results</a></li>
+            <li><a href="#">Average</a></li>
+            <li><a href="#">Nr of starts</a></li>
+            <li><a href="#">Events this year</a></li>
+          </ul>
+
+        </div>
+
+        <div class="listeLudvik">
+
+          <ul>
+            <li><p><a href="#">Mattis Hembre</a></p></li>
+            <li><a href="#">Best results</a></li>
+            <li><a href="#">Average</a></li>
+            <li><a href="#">Nr of starts</a></li>
+            <li><a href="#">Events this year</a></li>
+          </ul>
+
+        </div>
+
+        <div class="listeMattis">
+
+          <ul>
+            <li><p><a href="#">Ludvik Hembre</a></p></li>
+            <li><a href="#">Best results</a></li>
+            <li><a href="#">Average</a></li>
+            <li><a href="#">Nr of starts</a></li>
+            <li><a href="#">Events this year</a></li>
+          </ul>
+
+        </div>
+
+        <div class="listeTim">
+
+          <ul>
+            <li><p><a href="#"> Tim Leo Hembre</a></p></li>
+            <li><a href="#">Best results</a></li>
+            <li><a href="#">Average</a></li>
+            <li><a href="#">Nr of starts</a></li>
+            <li><a href="#">Events this year</a></li>
+          </ul>
+
+        </div>
+
+          <ul>
+            <li><p id="footer_contact"><a href="#">Contact Us</a></p></li>
+          </ul>
+
+      </div>
+
+      <div class="footer-container-two">
+
+
+        <div class="contact">
+
+          <p>Mail: pal.hembre@gmail.com</p>
+          <p>Phone: +47 42069666</p>
+          <p>Fax: +47 323 555 1234</p>
+
+        </div>
+
+        <p></p>
+
+        <div class="nasjonalStat">
+
+          <a href="#">Link til nasjonal pistol statistikk: fsu.no</a>
+
+        </div>
+
+
+      </div>
+
+    </footer>`)
