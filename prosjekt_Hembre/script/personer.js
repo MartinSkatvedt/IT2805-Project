6ec4452391764e410b8a1b61f0db4f1dbf23a114
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-10-21 18:23:31 +0200

    .

--- prosjekt_Hembre/script/personer.js
+++ prosjekt_Hembre/script/personer.js
@@ -1,3 +1,12 @@
-var pageURL = window.location.href;
-var lastURLSegment = pageURL.substr(pageURL.lastIndexOf('/') + 1);
-console.log(lastURLSegment);
+// var pageURL = window.location.href;
+// var lastURLSegment = pageURL.substr(pageURL.lastIndexOf('/') + 1);
+// console.log(lastURLSegment);
+
+
+
+//For å siste url del
+let url_string = window.location.href; //window.location.href
+let url = new URL(url_string);
+let id = url.searchParams.get("id");
+console.log(id);
+
