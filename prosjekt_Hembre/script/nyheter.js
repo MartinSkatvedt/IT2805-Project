02f936d85407f578d7940efa1a2f97af2574aeb7
Egil Hagen <egilhage@stud.ntnu.no>
2020-10-28 18:53:08 +0100

    Egil er boomer

---
+++ prosjekt_Hembre/script/nyheter.js
@@ -0,0 +1,36 @@
+
+const bilder = {
+    
+    slideShow:[
+        'bilder/om1.jpg',
+        'bilder/om2.jpg',
+        'bilder/om4.jpg',
+        'bilder/om5.jpg',
+        'bilder/om6.jpg',
+        'bilder/om7.jpg'
+    ],
+}
+
+const news = [
+    {
+        img: "bilder/om oss....",
+
+        text: [
+            'fsdfksamfsfmagdfsagmsa',
+            'sfsaffsaf'
+        ]
+
+        article:{
+            news_headline:"?????"
+            news_text: "?????"
+
+        },
+
+        class: "box_img",
+            
+        imgage:{
+            ??????
+        },
+
+    },
+]
