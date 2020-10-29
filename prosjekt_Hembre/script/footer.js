f16021e113e2eb04c4adaa5b7ff83010d6cf0d2a
Casper Amtrup <casperamtrup@dhcp-10-22-9-229.wlan.ntnu.no>
2020-10-29 09:48:42 +0100

    Endret footer.js

--- prosjekt_Hembre/script/footer.js
+++ prosjekt_Hembre/script/footer.js
@@ -2,61 +2,6 @@ document.write(`
     <footer>
 
       <div class="footer-container-one">
-
-        <img class="logo_footer" src="bilder/logo2.svg" alt="">
-
-        <div class="liste0 listeFooter">
-
-          <ul>
-            <li><p><a href="0">Pål Hembre</a></p></li>
-            <li><a href="1">Best results</a></li>
-            <li><a href="2">Average</a></li>
-            <li><a href="3">Nr of starts</a></li>
-            <li><a href="4">Events this year</a></li>
-          </ul>
-
-        </div>
-
-        <div class="liste1 listeFooter">
-
-          <ul>
-            <li><p><a href="0">Mattis Hembre</a></p></li>
-            <li><a href="1">Best results</a></li>
-            <li><a href="2">Average</a></li>
-            <li><a href="3">Nr of starts</a></li>
-            <li><a href="4">Events this year</a></li>
-          </ul>
-
-        </div>
-
-        <div class="liste2 listeFooter">
-
-          <ul>
-            <li><p><a href="0">Ludvik Hembre</a></p></li>
-            <li><a href="1">Best results</a></li>
-            <li><a href="2">Average</a></li>
-            <li><a href="3">Nr of starts</a></li>
-            <li><a href="4">Events this year</a></li>
-          </ul>
-
-        </div>
-
-        <div class="liste3 listeFooter">
-
-          <ul>
-            <li><p><a href="0"> Tim Leo Hembre</a></p></li>
-            <li><a href="1">Best results</a></li>
-            <li><a href="2">Average</a></li>
-            <li><a href="3">Nr of starts</a></li>
-            <li><a href="4">Events this year</a></li>
-          </ul>
-
-        </div>
-
-          <ul>
-            <li><p id="footer_contact"><a href="#">Contact Us</a></p></li>
-          </ul>
-
       </div>
 
       <div class="footer-container-two">
@@ -83,14 +28,42 @@ document.write(`
 
     </footer>`)
 
-    function editLink(){
-      for (let i in skyttere){
-        let links = document.querySelector(`.liste${i}`).querySelectorAll("a")
-          links[0].href = "skyttere.html?skytter="+ skyttere[i].id
-          links[1].href = skyttere[i].stasticsLinks.bestResults[0]
-          links[2].href = skyttere[i].stasticsLinks.average[0]
-          links[3].href = skyttere[i].stasticsLinks.totalStarts[0]
-          links[4].href ="#"
+    function makeFooter(){
+      let footer = document.querySelector(".footer-container-one")
+      footer.innerHTML = ""
+      footer.innerHTML += `
+
+      <img class="logo_footer" src="bilder/logo2.svg" alt="">
+
+
+      `
+
+      for(i in skyttere){
+        footer.innerHTML += `
+
+        <div class="liste${i} listeFooter">
+
+          <ul>
+            <li><p><a href="skyttere.html?skytter=${skyttere[i].id}">${skyttere[i].about.surName} ${skyttere[i].about.lastName}</a></p></li>
+            <li><a href="${skyttere[i].stasticsLinks[0][0]}">${skyttere[i].stasticsLinks[0][1]}</a></li>
+            <li><a href="${skyttere[i].stasticsLinks[1][0]}">${skyttere[i].stasticsLinks[1][1]}</a></li>
+            <li><a href="${skyttere[i].stasticsLinks[2][0]}">${skyttere[i].stasticsLinks[2][1]}</a></li>
+            <li><a href="${skyttere[i].stasticsLinks[3][0]}">${skyttere[i].stasticsLinks[3][1]}</a></li>
+          </ul>
+
+        </div>
+
+
+        `
       }
+      footer.innerHTML += `
+        </div>
+
+          <ul>
+            <li><p id="footer_contact"><a href="./kontakt.html">Contact Us</a></p></li>
+          </ul>
+
+        </div>
+      `
     }
-    editLink()
+    makeFooter()
