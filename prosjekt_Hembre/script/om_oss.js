6a8a2f725cfe57cf09b469ff13c1416012a9e6d1
Mattis Czternasty Hembre <mattisczthem@gmail.com>
2020-11-02 14:59:44 +0100

    .

--- prosjekt_Hembre/script/om_oss.js
+++ prosjekt_Hembre/script/om_oss.js
@@ -1,11 +1,14 @@
 //let navn_header = document.querySelectorAll(".navn_header");
-let navn_header = document.querySelector(".navn_header");
 
-const div = document.createElement('div')
+function makeNavnHeader() {
+  
+  const navn_header = document.querySelector(".navn_header");
+  
+  const div = document.createElement('div')
   for (let i = 0; i < skyttere.length; i++) {
     const skytter = skyttere[i];
     skytter.className = 'linker'
-
+    
     if (skytter.isShown) {
       const a = makePersonLink(skytter.id)
       a.className = 'link'
@@ -13,6 +16,20 @@ const div = document.createElement('div')
     }
   }
   navn_header.appendChild(div)
+}
 
 
-  console.log(div);
+
+function getTotalMealds() {
+  let gold = 0
+  let silver = 0
+  let bronse = 0
+  for (let i = 0; i < skyttere.length; i++) {
+    for (const merit of skyttere[i].merits) {
+      gold += merit.results.ind.gold + merit.results.team.gold
+      silver += merit.results.ind.silver + merit.results.team.silver
+      bronse += merit.results.ind.bronse + merit.results.team.bronse
+    }
+  }
+  return [gold,silver,bronse]
+}
