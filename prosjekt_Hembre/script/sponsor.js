1fece689bc421c48231657f0cee0b33038c68e01
Your Name <you@example.com>
2020-11-03 12:55:45 +0100

    Endret sponsor

--- prosjekt_Hembre/script/sponsor.js
+++ prosjekt_Hembre/script/sponsor.js
@@ -2,13 +2,15 @@ var bakkevold = document.querySelector("#bakkevold")
 var oxford = document.querySelector("#oxford")
 var bakkevold_info = document.querySelector("#bakkevold_info")
 var oxford_info = document.querySelector("#oxford_info")
-
+var contact_click = document.querySelector(".spons_contact")
 
 bakkevold.onclick=()=>bakkevoldS()
 oxford.onclick=()=>oxfordS()
 
 bakkevold_info.onclick=()=>goToBakkevold()
 oxford_info.onclick=()=>goToOxford()
+
+contact_click.onclick=()=>goToContact()
 
 function oxfordS(){
   oxford_info.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
@@ -22,4 +24,7 @@ function goToBakkevold(){
 }
 function goToOxford(){
   window.open("https://www.oxfordguncompany.co.uk/","_blank")
+}
+function goToContact(){
+  window.open("./kontakt.html","_blank")
 }
